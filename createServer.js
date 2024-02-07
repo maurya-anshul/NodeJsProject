43735de64e9fbf2cebe13d1871dd25793c01d753
maurya-anshul <mauryanshul93@gmail.com>
2024-02-07 18:44:34 +0530

    express.js middleware added

---
+++ createServer.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const express=require("express");
+const app=express();
+app.use((req,res,next)=>{
+    console.log('In middleware')
+    next();
+})
+app.use((req,res,next)=>{
+    console.log('In another middleware')
+})
+const server = http.createServer(app);
+server.listen(4000);
